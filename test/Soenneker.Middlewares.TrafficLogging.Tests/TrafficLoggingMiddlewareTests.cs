b6143e1565e9b32a9bd3e713466352792f013cef
Jake Soenneker <jake@soenneker.com>
2025-03-28 10:22:19 -0500

    Tests

--- test/Soenneker.Middlewares.TrafficLogging.Tests/TrafficLoggingMiddlewareTests.cs
+++ test/Soenneker.Middlewares.TrafficLogging.Tests/TrafficLoggingMiddlewareTests.cs
@@ -1,5 +1,4 @@
-﻿using Soenneker.Middlewares.TrafficLogging.Abstract;
-using Soenneker.Tests.FixturedUnit;
+﻿using Soenneker.Tests.FixturedUnit;
 using Xunit;
 
 namespace Soenneker.Middlewares.TrafficLogging.Tests;
@@ -7,11 +6,8 @@ namespace Soenneker.Middlewares.TrafficLogging.Tests;
 [Collection("Collection")]
 public class TrafficLoggingMiddlewareTests : FixturedUnitTest
 {
-    private readonly ITrafficLoggingMiddleware _util;
-
     public TrafficLoggingMiddlewareTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
     {
-        _util = Resolve<ITrafficLoggingMiddleware>(true);
     }
 
     [Fact]
