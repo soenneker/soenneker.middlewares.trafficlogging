692d3a34256d875551f4be3182df78cff0d431f4
Jake Soenneker <jake@soenneker.com>
2025-03-28 10:23:51 -0500

    raw string

--- src/TrafficLoggingMiddleware.cs
+++ src/TrafficLoggingMiddleware.cs
@@ -41,22 +41,17 @@ public class TrafficLoggingMiddleware : ITrafficLoggingMiddleware
         //Copy the contents of the new memory stream (which contains the response) to the original stream, which is then returned to the client.
         await context.Request.Body.CopyToAsync(requestStream).NoSync();
 
-        _logger.LogInformation(@"HTTP Request Information: {newLine}
-                                            Schema: {scheme}
-                                            Host: {host} 
-                                            Path: {path} 
-                                            Status: {status} 
-                                            QueryString: {queryString} 
-                                            Request Body: {body}
-                                            Headers: {headers}",
-            Environment.NewLine,
-            context.Request.Scheme,
-            context.Request.Host,
-            context.Request.Path,
-            context.Response.StatusCode,
-            context.Request.QueryString,
-            JsonUtil.Serialize(context.Request.Headers),
-            ReadStreamInChunks(requestStream));
+        _logger.LogInformation("""
+                               HTTP Request Information: {newLine}
+                                    Schema: {scheme}
+                                    Host: {host} 
+                                    Path: {path} 
+                                    Status: {status} 
+                                    QueryString: {queryString} 
+                                    Request Body: {body}
+                                    Headers: {headers}
+                               """, Environment.NewLine, context.Request.Scheme, context.Request.Host, context.Request.Path, context.Response.StatusCode,
+            context.Request.QueryString, JsonUtil.Serialize(context.Request.Headers), ReadStreamInChunks(requestStream));
 
         context.Request.Body.Position = 0;
     }
@@ -71,23 +66,18 @@ public class TrafficLoggingMiddleware : ITrafficLoggingMiddleware
         string? text = await new StreamReader(context.Response.Body).ReadToEndAsync().NoSync();
         context.Response.Body.ToStart();
 
-        _logger.LogInformation(@"HTTP Request Information: {newLine}
-                                            Schema: {scheme}
-                                            Host: {host} 
-                                            Path: {path} 
-                                            Status: {status} 
-                                            QueryString: {queryString},
-                                            Headers: {headers}
-                                            Request Body: {body},
-",
-            Environment.NewLine,
-            context.Request.Scheme,
-            context.Request.Host,
-            context.Request.Path,
-            context.Response.StatusCode,
-            context.Request.QueryString,
-            JsonUtil.Serialize(context.Response.Headers),
-            text);
+        _logger.LogInformation("""
+                               HTTP Request Information: {newLine}
+                                    Schema: {scheme}
+                                    Host: {host} 
+                                    Path: {path} 
+                                    Status: {status} 
+                                    QueryString: {queryString},
+                                    Headers: {headers}
+                                    Request Body: {body},
+
+                               """, Environment.NewLine, context.Request.Scheme, context.Request.Host, context.Request.Path, context.Response.StatusCode,
+            context.Request.QueryString, JsonUtil.Serialize(context.Response.Headers), text);
 
         //Copy the contents of the new memory stream (which contains the response) to the original stream, which is then returned to the client.
         await responseBody.CopyToAsync(originalBodyStream);
